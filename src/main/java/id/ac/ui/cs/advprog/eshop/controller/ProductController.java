347eba5fc8bee1626a19e2916b302d63876d5d45
zanetaoctoria <rebeccaocto@gmail.com>
2025-02-18 11:37:53 +0700

    push all

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -29,7 +29,7 @@ public class ProductController {
         return "redirect:list";
     }
 
-    @GetMapping("/List")
+    @GetMapping("/list")
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
