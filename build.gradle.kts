56820d40a6f66fbf8b4aa4dc0d32f92677d8fa6a
zanetaoctoria <rebeccaocto@gmail.com>
2025-02-20 00:08:17 +0700

    Testing in Spring Boot, Preparation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
+
 plugins {
     java
     id("org.springframework.boot") version "3.4.2"
@@ -32,8 +38,33 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+
 }
 
-tasks.withType<Test> {
+tasks.register<Test>(name = "unitTest") {
+    description = "Runs unit tests."
+    group = "verification"
+
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+}
+
+tasks.register<Test>(name = "functionalTest") {
+    description = "Runs functional tests."
+    group = "verification"
+
+    filter {
+        includeTestsMatching("*FunctionalTest")
+    }
+}
+
+
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
