30247c7c04d78e8ab2f183ad0e79c21c55f76ef3
zanetaoctoria <rebeccaocto@gmail.com>
2025-02-20 00:35:33 +0700

    ProductRepositoryTest

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -0,0 +1,72 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InjectMocks;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductRepositoryTest {
+
+    @InjectMocks
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void testCreateAndFind() {
+        Product product = new Product();
+        product.setProductId("123e4567-e89b-12d3-a456-556642440000");
+        product.setProductName("Product 1");
+        product.setProductQuantity(100);
+        productRepository.create(product);
+
+        Iterator<Product> products = productRepository.findAll();
+        assertTrue(products.hasNext());
+
+        Product savedProduct = products.next();
+
+        assertEquals(product.getProductId(), savedProduct.getProductId());
+        assertEquals(product.getProductName(), savedProduct.getProductName());
+        assertEquals(product.getProductQuantity(), savedProduct.getProductQuantity());
+    }
+
+    @Test
+    void testFindAllIfEmpty() {
+        Iterator<Product> products = productRepository.findAll();
+        assertFalse(products.hasNext());
+    }
+
+    @Test
+    void testFindAllIfMoreThanOneProduct() {
+        Product product1 = new Product();
+        product1.setProductId("123e4567-e89b-12d3-a456-556642440000");
+        product1.setProductName("Product 1");
+        product1.setProductQuantity(100);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("123e4567-e89b-12d3-a456-556642440001");
+        product2.setProductName("Product 2");
+        product2.setProductQuantity(200);
+        productRepository.create(product2);
+
+        Iterator<Product> products = productRepository.findAll();
+        assertTrue(products.hasNext());
+
+        Product savedProduct = products.next();
+        assertEquals(product1.getProductId(), savedProduct.getProductId());
+
+        savedProduct = products.next();
+        assertEquals(product2.getProductId(), savedProduct.getProductId());
+
+        assertFalse(products.hasNext());
+    }
+}
