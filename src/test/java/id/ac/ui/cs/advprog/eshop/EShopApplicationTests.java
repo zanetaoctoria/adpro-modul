e6f2befb3bea1f13d2bc651038dd09282ba26fa7
zanetaoctoria <rebeccaocto@gmail.com>
2025-02-20 13:02:53 +0700

    refining the coverage

--- src/test/java/id/ac/ui/cs/advprog/eshop/EShopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EShopApplicationTests.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EShopApplicationTests {
+class EshopApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void testMainMethod() {
+        // Calling the main method. If it executes without errors, the test passes.
+        EShopApplication.main(new String[]{});
+    }
 }
